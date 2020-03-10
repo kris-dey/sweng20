e62d4034d0abb78a442269706bb39ceabd789a56
byrnec68 <byrnec68@tcd.ie>
2020-03-10 16:19:29 +0000

    Update App.js
    Removed area beneath line

--- visualiser/src/App.js
+++ visualiser/src/App.js
@@ -7,16 +7,18 @@ import './App.css';
 export default class App extends Component{
   constructor(props){
     super(props)
-    for(var i = 1;i<1001;i++){
-      
-    }
+  var j =[1,2,3,4,5,6,7,8,9,10]    
+
     this.state = {
       data:{
-        labels:[1,2,3,4,5,6,7,8,9, ],
+        
+        labels:j,
         datasets:[
          {
            label: "Intensity",
            backgroundColor:"rgba(0, 255, 0, 0.75)",
+           borderColor:"rgba(0, 255, 0, 0.75)",
+           fill:false,
            data:[0.8,0.5,0.9,1,0.7,0.2,0.6,0.3, 0, 0.4]
          }
         ]
@@ -30,6 +32,7 @@ export default class App extends Component{
         <Line
         options={{
           responsive: true
+          
         }}
         data={this.state.data}
         />
