c1ef729fa7c7deef77338e7d3ab53e419e89b2ec
byrnec68 <byrnec68@tcd.ie>
2020-03-11 18:38:05 +0000

    Update App.js
    Changed line graph to more accurately look like final product, takes 1000 frames

--- visualiser/src/App.js
+++ visualiser/src/App.js
@@ -7,25 +7,43 @@ import './App.css';
 export default class App extends Component{
   constructor(props){
     super(props)
-  var j = [1,2,3,4,5,6,7,8,9,10] 
+  var j = [1,2,3,4,5,6,7,8,9,10];
+  var k =[]; var y = []; var z = [];
+  var x = 1;
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  while(x<1000){
+    k.push(x);x++;
+    y.push(getRandomArbitrary(0.4,1))
+    z.push(getRandomArbitrary(0,0.6))
+  }
+  
+  for(var i =0;i < j.length;i++){
+    j[i] = "Frame " + j[i];
+  }
+  j = k
+  
+
     this.state = {
       lineData:{
         
         labels:j,
         datasets:[
          {
-           label: "Region 1",
+           label: "Intensity Region 1",
            backgroundColor:"rgba(0, 255, 0, 0.75)",
            borderColor:"rgba(0, 255, 0, 0.75)",
            fill:false,
-           data:[0.8,0.5,0.9,1,0.7,0.2,0.6,0.3, 0, 0.4]
+           data:y//[0.8,0.5,0.9,1,0.7,0.2,0.6,0.3, 0, 0.4]
          },
          {
-          label: "Region 2",
+          label: "Intensity Region 2",
           backgroundColor:"rgba(255, 0, 0, 0.75)",
           borderColor:"rgba(255, 0, 0, 0.75)",
           fill:false,
-          data:[0.8,0.2,1,0.5,0.1,0.7,0.6,0.5, 0.6, 0.2]
+          data:z
         }
         ]
       },
