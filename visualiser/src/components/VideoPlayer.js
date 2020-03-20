c0bf447281443dc9ed8fdfc1eaf06a46a461cc3c
EmmaD194 <60703785+EmmaD194@users.noreply.github.com>
2020-03-20 12:25:39 +0000

    trying to fix button hover

--- visualiser/src/components/VideoPlayer.js
+++ visualiser/src/components/VideoPlayer.js
@@ -1,31 +1,43 @@
 import React, { Component } from "react";
 import videos from "./sampleVideo.mp4";
 import classes from "./VideoPlayer.module.css";
-import Button from "./button"
+import Button from "./button";
 
 let renderBoxes = [
   {
-    right: 250, bottom: 500, width: 100, height: 100, onClick: () => {
-      alert(document.getElementById("surgeryVideo").currentTime)
+    right: 250,
+    bottom: 500,
+    width: 100,
+    height: 100,
+    onClick: () => {
+      alert(document.getElementById("surgeryVideo").currentTime);
       // console.log(document.getElementById("surgeryVideo").currentTime)
       // alert("BTN ONE")
     }
   },
   {
-    right: 200, bottom: 620, width: 60, height: 40, onClick: () => {
-      alert(document.getElementById("surgeryVideo").currentTime)
+    right: 200,
+    bottom: 620,
+    width: 60,
+    height: 40,
+    onClick: () => {
+      alert(document.getElementById("surgeryVideo").currentTime);
       // console.log(document.getElementById("surgeryVideo").currentTime)
       // alert("BTN TWO")
     }
   },
   {
-    right: 280, bottom: 300, width: 180, height: 100, onClick: () => {
-      alert(document.getElementById("surgeryVideo").currentTime)
+    right: 280,
+    bottom: 300,
+    width: 180,
+    height: 100,
+    onClick: () => {
+      alert(document.getElementById("surgeryVideo").currentTime);
       // console.log(document.getElementById("surgeryVideo").currentTime)
       // alert("BTN THREE")
     }
   }
-]
+];
 
 class VideoPlayer extends Component {
   render() {
@@ -39,11 +51,11 @@ class VideoPlayer extends Component {
           src={videos}
           frameBorder="0"
           controls
-          // allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></video>
         {renderBoxes.map(e => {
-          return <Button params={e} />
+          return <Button params={e} />;
         })}
         {/* <Button params={{ right: 200, bottom: 600, width: 180, height: 100 }} /> */}
       </div>
--- visualiser/src/components/button.js
+++ visualiser/src/components/button.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import classes from "./VideoPlayer.module.css";
 // import classes from "./VideoPlayer.module.css";
 // import { Container, Button, Link } from 'react-floating-action-button'
 
@@ -27,20 +28,20 @@ class ButtonBoi extends Component {
       width: this.props.params.width,
       height: this.props.params.height,
       onClick: this.props.params.onClick
-    }
+    };
     this.subTstFloat = {
       right: parseInt(this.state.right),
       bottom: parseInt(this.state.bottom),
       margin: 10,
-      top: 'auto',
-      left: 'auto',
-      position: 'fixed',
+      top: "auto",
+      left: "auto",
+      position: "fixed",
       paddingRight: parseInt(this.state.width),
       paddingBottom: parseInt(this.state.height),
-      paddingLeft: '0px',
-      paddingTop: '0px',
-      backgroundColor: '#f0f0'
-    }
+      paddingLeft: "0px",
+      paddingTop: "0px",
+      backgroundColor: "#f0f0"
+    };
     // console.log(this.state)
     // console.log(this.subTstFloat);
   }
@@ -48,7 +49,11 @@ class ButtonBoi extends Component {
   render() {
     return (
       <div>
-        <button style={this.subTstFloat} onClick={this.state.onClick}></button>
+        <button
+          className={classes.button}
+          style={this.subTstFloat}
+          onClick={this.state.onClick}
+        ></button>
         {/* <button style={btnFloatStyle}>tstBoi</button> */}
       </div>
     );
