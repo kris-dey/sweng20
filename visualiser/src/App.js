ba74fd7ce3774c6ff488c95229a5c4a7dce90ce8
darts <darts@tcd.ie>
2020-03-22 19:03:19 +0000

    Passing parameters

--- visualiser/src/App.js
+++ visualiser/src/App.js
@@ -9,17 +9,56 @@ import LineGraph from './lineGraph'
 //import logo from './logo.svg';
 import './App.css';
 
+
+const barData = {
+  labels: ['ROI 1', 'ROI 2', 'ROI 3'],
+  datasets: [{
+    label: 'Healthy',
+    backgroundColor: "rgba(0, 255, 0, 0.75)",
+    data: [50, 40, 50],
+  }, {
+    label: 'Benign',
+    backgroundColor: "rgba(0, 0, 255, 0.75)",
+    data: [20, 20, 30],
+  }, {
+    label: 'Cancerous',
+    backgroundColor: "rgba(255, 0, 0, 0.75)",
+    data: [30, 40, 20],
+  }]
+}
+
+const options = {
+  title: {
+    display: true,
+
+  },
+  tooltips: {
+    mode: 'index',
+    intersect: false
+  },
+  responsive: true,
+  scales: {
+    xAxes: [{
+      stacked: true,
+    }],
+    yAxes: [{
+      stacked: true
+    }]
+  }
+}
+
+
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      barHealthy: [50,40,50],
-      barBenign: [20,20,30,],
-      barCancerous: [30,40,20]
+      barHealthy: [50, 40, 50],
+      barBenign: [20, 20, 30],
+      barCancerous: [30, 40, 20]
       // tried to pass these to barChart but that throws an 'undefined error'.
 
     }
-    
+
 
   }
   render() {
@@ -34,24 +73,25 @@ export default class App extends Component {
       y.push(getRandomArbitrary(0.4, 1))
       z.push(getRandomArbitrary(0, 0.6))
     }
-    
+
 
     return (
       <div style={{ position: 'relative', width: 600, height: 550 }}>
-        
-         <LineGraph 
-         //this now draws but has no labels nor data despite it being hard coded in line graph
-        
+
+        <LineGraph
+        //this now draws but has no labels nor data despite it being hard coded in line graph
+
 
         />
 
         <h3>Bar Chart</h3>
-        
-         <BarChart
-         
-           
-         
-         
+
+        <BarChart
+
+          barData={barData}
+
+
+
         // 
         // data={this.state.barData}
 
--- visualiser/src/barChart.js
+++ visualiser/src/barChart.js
@@ -1,68 +1,72 @@
 import React, { Component } from 'react';
-import {Bar} from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 import App from './App';
 
-class BarChart extends Component{
-    constructor(props){
-        super(props)
-        //  function changeBarData(Healthy,Benign,Cancerous){
-        //   this.state.barData.datasets[0].data = Healthy;
-        //   this.state.barData.datasets[1].data = Benign;
-        //   this.state.barData.datasets[2].data = Cancerous;
-        // }
-        this.state={
-          barData:{
-            labels: ['ROI 1', 'ROI 2', 'ROI 3'],
-        	datasets: [{
-        		label: 'Healthy',
-        		backgroundColor: "rgba(0, 255, 0, 0.75)",
-        		data: App.props.barHealthy,
-        	}, {
-        		label: 'Benign',
-        		backgroundColor: "rgba(0, 0, 255, 0.75)",
-        		data: App.state.barBenign,
-        	}, {
-        		label: 'Cancerous',
-        		backgroundColor: "rgba(255, 0, 0, 0.75)",
-        		data: App.state.barCancerous,
-        	}]
-        },
-        options:{
-            title: {
-              display: true,
-  
-            },
-            tooltips: {
-              mode: 'index',
-              intersect: false
-            },
-            responsive: true,
-            scales: {
-              xAxes: [{
-                stacked: true,
-              }],
-              yAxes: [{
-                stacked: true
-              }]
-            }
-          }
-      }
-    }
+class BarChart extends Component {
+  constructor(props) {
+    super(props)
+    //  function changeBarData(Healthy,Benign,Cancerous){
+    //   this.state.barData.datasets[0].data = Healthy;
+    //   this.state.barData.datasets[1].data = Benign;
+    //   this.state.barData.datasets[2].data = Cancerous;
+    // }
+    this.state = {
+      barData: this.props.barData,
+      options: this.props.options
 
-    render() {
-  
-        return(
-            <div style= {{position: 'relative', width: 600, height:550}}>
-            <Bar
-           options={this.state.options}
-            data={this.state.barData}
-            
-  
-          /> </div>
-            
-        
-      )
+
+      //   barData:{
+      //     labels: ['ROI 1', 'ROI 2', 'ROI 3'],
+      // 	datasets: [{
+      // 		label: 'Healthy',
+      // 		backgroundColor: "rgba(0, 255, 0, 0.75)",
+      // 		data: App.props.barHealthy,
+      // 	}, {
+      // 		label: 'Benign',
+      // 		backgroundColor: "rgba(0, 0, 255, 0.75)",
+      // 		data: App.state.barBenign,
+      // 	}, {
+      // 		label: 'Cancerous',
+      // 		backgroundColor: "rgba(255, 0, 0, 0.75)",
+      // 		data: App.state.barCancerous,
+      // 	}]
+      // },
+      // options:{
+      //     title: {
+      //       display: true,
+
+      //     },
+      //     tooltips: {
+      //       mode: 'index',
+      //       intersect: false
+      //     },
+      //     responsive: true,
+      //     scales: {
+      //       xAxes: [{
+      //         stacked: true,
+      //       }],
+      //       yAxes: [{
+      //         stacked: true
+      //       }]
+      //     }
+      //   }
     }
+  }
+
+  render() {
+
+    return (
+      <div style={{ position: 'relative', width: 600, height: 550 }}>
+        <Bar
+          options={this.state.options}
+          data={this.state.barData}
+
+
+        /> </div>
+
+
+    )
+  }
 
 }
 export default BarChart
