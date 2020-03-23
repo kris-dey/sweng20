95461451679e9ce1d96c04f49e6f4a8c2614992e
EmmaD194 <60703785+EmmaD194@users.noreply.github.com>
2020-03-23 20:59:19 +0000

    box won't move

--- visualiser/src/components/button.js
+++ visualiser/src/components/button.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import classes from "./VideoPlayer.module.css";
+import { Animate } from "react-move";
+import _ from "lodash";
 // import classes from "./VideoPlayer.module.css";
 // import { Container, Button, Link } from 'react-floating-action-button'
 
@@ -19,7 +21,7 @@ import classes from "./VideoPlayer.module.css";
 //   right: '800px'
 // }
 
-class ButtonBoi extends Component {
+/*class ButtonBoi extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -54,6 +56,73 @@ class ButtonBoi extends Component {
           style={this.subTstFloat}
           onClick={this.state.onClick}
         ></button>
+      </div>
+    );
+  }
+}
+export default ButtonBoi;*/
+
+class ButtonBoi extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      right: 0,
+      color: "red",
+      duration: 300
+    };
+  }
+
+  toggleButtonPosition = () => {
+    let left = 0;
+    if (!this.state.left || this.state.left === 0) {
+      left = Math.random() * 300 + 50;
+    }
+    let duration = Math.random() * 300 + 200;
+    let color = _.sample(["red", "blue", "black", "green"]);
+    this.setState({
+      left,
+      color,
+      duration
+    });
+  };
+
+  render() {
+    let { left, color, duration } = this.state;
+    return (
+      <div>
+        <Animate
+          data={{
+            left: left,
+            color: color
+          }}
+          default={{
+            left: 0,
+            color: "red"
+          }}
+          duration={duration}
+          easing="easeQuadIn"
+        >
+          {data => (
+            <div
+              style={{
+                transform: `translate(${data.left}px, 0)`,
+                background: data.color || "red",
+                position: "absolute",
+                width: 100,
+                height: 50,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: 10,
+                color: "white",
+                cursor: "pointer"
+              }}
+              onClick={this.toggleButtonPosition}
+            >
+              Cancerous
+            </div>
+          )}
+        </Animate>
       </div>
     );
   }
