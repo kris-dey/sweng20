28f9ca3f7d938cdcfb8177b53d5d908b36c1ac61
Senán d'Art <darts@tcd.ie>
2020-04-06 17:51:41 +0100

    Revert "Senan wk10, responsive graphs "

--- visualiser/src/components/barChart.js
+++ visualiser/src/components/barChart.js
@@ -6,57 +6,50 @@ class BarChart extends Component {
   constructor(props) {
     super(props)
 
-    let labels = this.props.HArray.map((e, i) => e = `ROI:${i}`)
-    let health_data = this.props.HArray;
-    let benign_data = this.props.BArray;
-    let cancer_data = this.props.CArray;
+    let labels = []
+
+    let health_data = [];
+    let benign_data = [];
+    let cancer_data = [];
+
+    this.props.predictionData.filter( (data, index) =>
+      {
+
+        let total = (data.class_probabilities.Healthy + data.class_probabilities.Benign + data.class_probabilities.Cancer)/100
+
+        health_data.push(data.class_probabilities.Healthy/total);
+        benign_data.push(data.class_probabilities.Benign/total);
+        cancer_data.push(data.class_probabilities.Cancer/total);
+
+        var label = "ROI " + index.toString() + ": " + data.prediction;
+        labels.push(label);
+      }
+    )
+
+    console.log(health_data);
+    console.log(benign_data);
+    console.log(cancer_data);
+
 
     this.state = {
       barData: {
         labels: labels,
         datasets: [{
-          label: 'Healthy',
-          backgroundColor: "rgba(0, 255, 0, 0.75)",
-          data: health_data,
+            label: 'Healthy',
+            backgroundColor: "rgba(0, 255, 0, 0.75)",
+            data: health_data,
         }, {
-          label: 'Benign',
-          backgroundColor: "rgba(0, 0, 255, 0.75)",
-          data: benign_data,
+            label: 'Benign',
+            backgroundColor: "rgba(0, 0, 255, 0.75)",
+            data: benign_data,
         }, {
-          label: 'Cancerous',
-          backgroundColor: "rgba(255, 0, 0, 0.75)",
-          data: cancer_data,
+            label: 'Cancerous',
+            backgroundColor: "rgba(255, 0, 0, 0.75)",
+            data: cancer_data,
         }]
-      },
+    },
       options: this.props.options
     }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    let labels = nextProps.HArray.map((e, i) => e = `ROI:${i}`)
-    let health_data = nextProps.HArray;
-    let benign_data = nextProps.BArray;
-    let cancer_data = nextProps.CArray;
-
-    this.setState({
-      barData: {
-        labels: labels,
-        datasets: [{
-          label: 'Healthy',
-          backgroundColor: "rgba(0, 255, 0, 0.75)",
-          data: health_data,
-        }, {
-          label: 'Benign',
-          backgroundColor: "rgba(0, 0, 255, 0.75)",
-          data: benign_data,
-        }, {
-          label: 'Cancerous',
-          backgroundColor: "rgba(255, 0, 0, 0.75)",
-          data: cancer_data,
-        }]
-      },
-      options: nextProps.options
-    })
   }
 
   render() {
@@ -66,6 +59,46 @@ class BarChart extends Component {
         <Bar
           options={this.state.options}
           data={this.state.barData}
+        // SAMPLE PARAMS: DATA
+        //   barData:{
+        //     labels: ['ROI 1', 'ROI 2', 'ROI 3'],
+        // 	datasets: [{
+        // 		label: 'Healthy',
+        // 		backgroundColor: "rgba(0, 255, 0, 0.75)",
+        // 		data: [x,y,z],
+        // 	}, {
+        // 		label: 'Benign',
+        // 		backgroundColor: "rgba(0, 0, 255, 0.75)",
+        // 		data: [x,y,z],
+        // 	}, {
+        // 		label: 'Cancerous',
+        // 		backgroundColor: "rgba(255, 0, 0, 0.75)",
+        // 		data: [x,y,z],
+        // 	}]
+        // }
+
+
+        // OPTIONS: MUST STAY FIXED:
+        // options:{
+        //     title: {
+        //       display: true,
+
+        //     },
+        //     tooltips: {
+        //       mode: 'index',
+        //       intersect: false
+        //     },
+        //     responsive: true,
+        //     scales: {
+        //       xAxes: [{
+        //         stacked: true,
+        //       }],
+        //       yAxes: [{
+        //         stacked: true
+        //       }]
+        //     }
+        //   }
+
         /> </div>
 
 
--- visualiser/src/index.jsx
+++ visualiser/src/index.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-// import Export from './ExportScreenshot'
-import rawData from "./data/Output.json"
-// import intensityDataJSON from "./data/intensity_results.json"
+import Export from './ExportScreenshot'
+import intensityDataJSON from "./data/intensity_results.json"
 import predictionDataJSON from "./data/prediction_results.json"
 
 // const jsonifiedJSON = JSON.parse(dataJSON)
-const intensityData = rawData.map((e) => e.norm_times_series.map(a => a[0]))
-
-// console.log(intensityData)
-
-// const intensityData = intensityDataJSON
+const intensityData = intensityDataJSON
 const predictionData = predictionDataJSON
 
 ReactDOM.render(<App intensityData={intensityData} predictionData={predictionData} />, document.getElementById('root'))
